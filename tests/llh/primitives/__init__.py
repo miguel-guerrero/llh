ff678cc8206d26f6d8193ad0b72df6b1939eb551
Miguel Guerrero <ma_gm@yahoo.com>
2023-07-04 17:48:28 -0700

    created links under tests

--- tests/llh/primitives/__init__.py
+++
@@ -1,8 +0,0 @@
-from .And import And, f_and
-from .Or import Or, f_or
-from .Xor import Xor, f_xor
-from .Not import Not, f_not
-from .Const import Const, f_const
-from .Next import Next  # , f_next
-from .Selector import Selector, f_selector
-from .WireJoin import WireJoin, f_wirejoin
